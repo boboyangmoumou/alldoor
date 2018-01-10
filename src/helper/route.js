be226be315aa0f54dac5ecbbf51af2e1c33eb676
Yangjh01 <Yangjh01@mingyuanyun.com>
2018-01-10 17:58:40 +0800

    new update

--- src/helper/route.js
+++ src/helper/route.js
@@ -5,25 +5,41 @@ const Handlebars = require('handlebars');
 const stat = promisify(fs.stat);
 const readdir = promisify(fs.readdir);
 const config = require('../config/defaultConfig');
+const mime = require('./mime');
+const compress = require('./compress');
+const range = require('./range');
+
 
 const tplPath = path.join(__dirname,'../template/dir.tpl');
 const source = fs.readFileSync(tplPath);
 const template = Handlebars.compile(source.toString());
-
 module.exports = async function(req, res, filePath){
     try {
         const stats =await stat(filePath);
         if(stats.isFile()) {
+            const contentType = mime(filePath);
+            console.info(contentType);
             res.statusCode = 200;
-            res.setHeader('Content-Type', 'text/plain');
-            fs.createReadStream(filePath).pipe(res);
+            res.setHeader('Content-Type', contentType);
+            let rs;
+            const {code, start, end} = range(stats.size,req, res);
+            if(code === 200) {
+                rs = fs.createReadStream(filePath);
+            } else {
+                rs = fs.createReadStream(filePath, {start, end});
+            }
+            if (filePath.match(config.compress)) {
+                rs = compress(rs, req, res);
+            }
+            rs.pipe(res);
         }else if(stats.isDirectory()) {
             const files = await readdir(filePath);
             res.statusCode = 200;
             res.setHeader('Content-Type', 'text/html');
+            const dir = path.relative(config.root, filePath);
             const data={
                 title: path.basename(filePath), 
-                dir: path.relative(config.root, filePath),
+                dir: dir ? `/${dir}` : '',
                 files
             };
             res.end(template(data));
@@ -35,3 +51,10 @@ module.exports = async function(req, res, filePath){
         res.end(`${filePath} is not a directory or file\n ${ex.toString()}`);
     }
 }
+
+
+
+
+
+
+
--- src/template/dir.tpl
+++ src/template/dir.tpl
@@ -5,10 +5,24 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>{{title}}</title>
+    <style>
+        body {
+            margin: 30px;
+        }
+        a {
+            display: block;
+            font-size: 30px;
+            color: #333;
+            text-decoration: none;
+        }
+        a:hover{
+            color: coral;
+        }
+    </style>
 </head>
 <body>
     {{#each files}}
-        <a href="{{../dir}}/{{this}}">{{this}}</a>
+        <a href="{{../dir}}/{{files}}">{{files}}</a>
     {{/each}}
 </body>
 </html>
